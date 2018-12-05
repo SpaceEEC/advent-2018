179095a2c76565c822529a72fd71e6fde48ba749
SpaceEEC <wermail2002-kap@yahoo.de>
2018-12-05 20:13:30 +0100

    refactor(05-2): use a tuple of chars to drop instead of subtracting

--- 05/solution.exs
+++ 05/solution.exs
@@ -7,22 +7,22 @@ defmodule Solution do
     |> String.trim_trailing("\n")
   end
 
-  defp collapse_polymer(str1, str2 \\ "", ignore_char \\ nil)
+  defp collapse_polymer(str1, str2 \\ "", ignore_chars \\ nil)
 
-  defp collapse_polymer("", str2, ignore_char), do: str2 |> String.reverse()
+  defp collapse_polymer("", str2, _ignore_chars), do: str2 |> String.reverse()
 
-  defp collapse_polymer(<<c1::utf8, rest1::binary>>, str2, ignore_char)
-       when c1 in [ignore_char, ignore_char + ?A - ?a] do
-    collapse_polymer(rest1, str2, ignore_char)
+  defp collapse_polymer(<<c1::utf8, rest1::binary>>, str2, {ignore1, ignore2} = ignore_chars)
+       when c1 in [ignore1, ignore2] do
+    collapse_polymer(rest1, str2, ignore_chars)
   end
 
-  defp collapse_polymer(<<c1::utf8, rest1::binary>>, <<c2::utf8, rest2::binary>>, ignore_char)
+  defp collapse_polymer(<<c1::utf8, rest1::binary>>, <<c2::utf8, rest2::binary>>, ignore_chars)
        when (c1 - c2) in [?A - ?a, ?a - ?A] do
-    collapse_polymer(rest1, rest2, ignore_char)
+    collapse_polymer(rest1, rest2, ignore_chars)
   end
 
-  defp collapse_polymer(<<c1::utf8, rest1::binary>>, str2, ignore_char) do
-    collapse_polymer(rest1, <<c1::utf8, str2::binary>>, ignore_char)
+  defp collapse_polymer(<<c1::utf8, rest1::binary>>, str2, ignore_chars) do
+    collapse_polymer(rest1, <<c1::utf8, str2::binary>>, ignore_chars)
   end
 
   # 10978
@@ -37,9 +37,9 @@ defmodule Solution do
   def solve2(file \\ "input.txt") do
     composition = read_file(file)
 
-    for char <- ?a..?z do
+    for chars <- [?a..?z, ?A..?Z] |> Enum.zip() do
       composition
-      |> collapse_polymer("", char)
+      |> collapse_polymer("", chars)
       |> String.length()
     end
     |> Enum.min()

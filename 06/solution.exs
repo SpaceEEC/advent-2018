cb746507d45a7e84ee098d32b02bbfcf2986b4f7
SpaceEEC <wermail2002-kap@yahoo.de>
2018-12-06 20:47:06 +0100

    refactor(06-1): better solution

--- 06/solution.exs
+++ 06/solution.exs
@@ -1,31 +1,52 @@
 defmodule Solution do
+  defp solve(file) do
+    file
+    |> File.stream!()
+    |> Enum.map(fn line ->
+      ~r{(\d+), (\d+)}
+      |> Regex.run(line)
+      |> Enum.drop(1)
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple()
+    end)
+  end
+
+  # 3293
   def solve1(file \\ "test.txt") do
-    coordinates =
-      file
-      |> File.stream!()
-      |> Enum.map(fn line ->
-        ~r{(\d+), (\d+)}
-        |> Regex.run(line)
-        |> Enum.drop(1)
-        |> Enum.map(&String.to_integer/1)
-        |> List.to_tuple()
+    coordinates = solve(file)
+
+    {max_y, max_x, min_y, min_x} =
+      Enum.reduce(coordinates, {nil, nil, nil, nil}, fn
+        {x, y}, {nil, nil, nil, nil} ->
+          {y, x, y, x}
+
+        {x, y}, {max_y, max_x, min_y, min_x} ->
+          {max(y, max_y), max(x, max_x), min(y, min_y), min(x, min_x)}
       end)
 
-    extremes = get_extreme_coordinates(coordinates)
-
-    tmp = count_distances(coordinates, 0..extremes.max_y, 0..extremes.max_x)
-    tmp2 = count_distances(coordinates, -1..(extremes.max_y + 1), -1..(extremes.max_x + 1))
-
-    Map.merge(
-      tmp,
-      tmp2,
-      fn
-        _, x, x -> x
-        _, _x, _y -> nil
+    coords =
+      for {x_generator, y_generator} <- [
+            {[min_x, max_x], min_y..max_y},
+            {min_x..max_x, [min_y, max_y]}
+          ],
+          x <- x_generator,
+          y <- y_generator do
+        {x, y}
       end
-    )
+
+    tmp = count_distances(coordinates, min_y..max_y, min_x..max_x)
+
+    tmp2 =
+      tmp
+      |> Map.take(coords)
+      |> Map.values()
+      |> Enum.uniq()
+
+    tmp
     |> Map.values()
-    |> Enum.filter(& &1)
+    |> Enum.group_by(& &1)
+    |> Map.drop(tmp2)
+    |> Enum.map(&length(elem(&1, 1)))
     |> Enum.max()
   end
 
@@ -35,7 +56,7 @@ defmodule Solution do
       {x, y}
     end
     |> Task.async_stream(
-      fn {x1, y1} ->
+      fn {x1, y1} = coord ->
         for {x2, y2} = pair <- coordinates do
           {pair, abs(x1 - x2) + abs(y1 - y2)}
         end
@@ -45,53 +66,12 @@ defmodule Solution do
             nil
 
           [{pair, _dist} | _rest] ->
-            # {x1, y1}
-            pair
+            {coord, pair}
         end
       end,
       ordered: false
     )
-    |> Enum.group_by(&elem(&1, 1))
-    |> Map.delete(nil)
-    |> Map.new(fn {k, v} -> {k, length(v)} end)
+    |> Enum.filter(&elem(&1, 1))
+    |> Map.new(&elem(&1, 1))
   end
-
-  defp get_extreme_coordinates(result \\ %{}, list)
-  defp get_extreme_coordinates(result, []), do: result
-
-  defp get_extreme_coordinates(result, [{x, y} | rest]) do
-    result
-    |> Map.merge(
-      %{
-        min_x: x,
-        min_y: y
-      },
-      fn _, v1, v2 -> min(v1, v2) end
-    )
-    |> Map.merge(
-      %{
-        max_x: x,
-        max_y: y
-      },
-      fn _, v1, v2 -> max(v1, v2) end
-    )
-    |> get_extreme_coordinates(rest)
-  end
-
-  # defp get_extremes(extremes, list, acc \\ [])
-  # defp get_extremes(_extremes, [], acc), do: acc
-
-  # defp get_extremes(
-  #        %{max_x: x2, max_y: y2, min_x: x3, min_y: y3} = extremes,
-  #        [{x1, y1} = coordinates | rest],
-  #        acc
-  #      )
-  #      when x1 in [x2, x3]
-  #      when y1 in [y2, y3] do
-  #   get_extremes(extremes, rest, [coordinates | acc])
-  # end
-
-  # defp get_extremes(extremes, [_coordinates | rest], acc) do
-  #   get_extremes(extremes, rest, acc)
-  # end
 end
